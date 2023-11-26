882e3aea8976902a60767ddec622fbc4a1f396e0
alex-artamonov <alex.artamonov2010@gmail.com>
2023-11-26 23:33:20 +0100

    clean up read_web

--- read_web.py
+++ read_web.py
@@ -12,21 +12,3 @@ def read_conj(verb):
     soup = BeautifulSoup(html, "lxml")
     return soup.find_all("table", class_="neoConj")
 
-
-# print(soup.head.title.contents)
-# print(soup.body.a.text)
-# print(soup.find_all('h3')[0].next)
-# print(soup.find_all('h4'))
-
-# for ele in sp:
-#     print(ele.text)
-
-
-# result = []
-
-# with open("./result.html", "w") as file:
-#     for ele in h4:
-#         # print(ele.text)
-#         file.write(str(ele))
-
-# print(result, len(result))
