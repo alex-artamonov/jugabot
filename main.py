cf254340b519c966be1d314e574d7fbd062dcfba
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-13 16:18:52 +0100

    comment print in main.py and read_web.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from telegram.constants import ParseMode
 from telebot import types
 import conj_funcs as cf
 import keys as ks
-# import config
 import os
 
 TOKEN = os.environ["JUGABOT"]
-print(TOKEN)
 bot = telebot.TeleBot(TOKEN)
 commands = [f"/{ele}" for ele in ks.tiempos_list()]
 current_verbs = {}
--- read_web.py
+++ read_web.py
@@ -7,7 +7,7 @@ def read_conj(verb):
     """Returns the table of conjugation of a given Spanish verb"""
 
     query_string = f"https://www.wordreference.com/conj/esverbs.aspx?v={verb}"
-    print(query_string)
+    # print(query_string)
     html = requests.get(query_string).content
     soup = BeautifulSoup(html, "lxml")
     return soup.find_all("table", class_="neoConj")
